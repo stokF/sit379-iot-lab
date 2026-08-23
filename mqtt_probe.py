829d74c4b11c6b59de73b2b2aae4e44266615f82
stokF <finnh990@gmail.com>
2026-08-23 12:40:29 +1000

    readability and functions

--- mqtt_probe.py
+++ mqtt_probe.py
@@ -1,12 +1,14 @@
 import argparse
 import sys 
 import time
+from tkinter import UNDERLINE
 
 
-ERROR = '\033[91m'
-WARNING = '\033[93m'
-NOTIFICATION = '\033[96m'
+ERROR = '\033[91m [!!!]\033[0m'
+WARNING = '\033[93m [X]\033[0m'
+NOTIFICATION = '\033[96m[+]\033[0m'
 COLOUR_END = '\033[0m'
+UNDERLINE = '\033[4m'
 
 try:
     import paho.mqtt.client as mqtt
@@ -29,32 +31,32 @@ def on_connect(client, userdata, flags, reasonCode, properties=None):
         print (f"{NOTIFICATION}Connected to {brokerHost}:{brokerPort} anonymously - no authentication used")
         client.subscribe("$SYS/#", qos=0)
         print(f"Wildcard '#' subscribed.")
-        print (f"Topics being sent now.{COLOUR_END}")
+        print (f"Topics being sent now.")
     else:
-        print(f"{WARNING} MQTT Broker unable to connect on 1883 ({reasonCode}){COLOUR_END}")
+        print(f"\n{WARNING} MQTT Broker unable to connect on 1883 ({reasonCode})")
         sys.exit(1)
 
 def on_message(client, userdata, msg):
     ts = time.strftime("%H:%M:%S")
     topic = msg.topic
     payload = msg.payload.decode("utf-8", errors="replace")
-    print(f"{NOTIFICATION}[{ts}] - {topic:<35}:{payload}{COLOUR_END}")
+    print(f"{NOTIFICATION}[{ts}] - {topic:<35}:{payload}")
 
     topicSet.add(topic)
     msg.append((ts, topic, payload))
 
 def on_disconnect(client, userdata, disconnect_flags, reasonCode, properties=None):
         if reasonCode != 0:
-            print(f"{WARNING}Unexpected disconnect ({reasonCode}){COLOUR_END}")
+            print(f"{WARNING}Unexpected disconnect ({reasonCode})")
 
             try:
                 client.connect(brokerHost, brokerPort, keepalive=60)
             except Exception as e:
-                print(f"{ERROR}Connection failed: {e}{COLOUR_END}")
+                print(f"\n{ERROR}Connection failed: {e}")
 
 def main():
     parser = argparse.ArgumentParser(description="Anonymous Mosquitto client (MQTT) probe")
-    parser.add_argument("time", type=int, default=harvestSecs, help=f"{NOTIFICATION}Harvest duration in seconds: {harvestSecs}{COLOUR_END}")
+    parser.add_argument("time", type=int, default=harvestSecs, help=f"{NOTIFICATION}Harvest duration in seconds: {harvestSecs}")
     args = parser.parse_args()
 
     client = mqtt.Client(
@@ -65,9 +67,33 @@ def main():
     client.on_message = on_message
     client.on_disconnect = on_disconnect 
 
-    print(f"{NOTIFICATION}Loading connection to: {brokerHost}/{brokerPort}{COLOUR_END}")
+    print(f"{NOTIFICATION}Loading connection to: {brokerHost}/{brokerPort}")
     try:
         client.connect(brokerHost, brokerPort, keepalive=30)
     except OSError as e:
-        print(f"{ERROR}Connection dropped, timeout increasing to 60 seconds.{e}{COLOUR_END}")
+        print(f"\n{ERROR}Connection dropped, timeout increasing to 60 seconds.{e}")
         sys.exit(1)
+
+    client.loop_start()
+
+    print(f"{NOTIFICATION}Harvesting for {args.time}s")
+    print(f"{UNDERLINE}Enter 'CTRL + C' to terminate early.")
+    try:
+        time.sleep(args.time)
+    except KeyboardInterrupt:
+        print(f"\n{WARNING}Harvest attempt forcefully terminated.")
+
+    with open(harvestFile, "w") as f:
+        f.write(f"{NOTIFICATION}MQTT # Harvest completed - Duration: {args.time}s")
+        f.write(f"{NOTIFICATION}# Broker: {brokerHost}/{brokerPort}")
+        for ts, topic, payload in msg:
+            f.write(f"{ts} {topic} {payload}\n")
+        print(f"{NOTIFICATION} Harvest data placed in {harvestFile}\n")
+        print(f"{NOTIFICATION} {msg} messages saved.")
+
+    with open(topicMap_File, "w") as f:
+        
+            
+
+
+
